13f0de3f964f520d696ad55598af7ae821dbba2a
s8189882 <s8189882@gmail.com>
2022-09-02 16:39:37 +0530

    Add master

---
+++ Generics Print Array/src/generics/print/array/PrintArray.java
@@ -0,0 +1,12 @@
+package generics.print.array;
+
+/*Given an array of Integer, Double and Character, write a program to print the same
+- Create PrintArray class and define toPrint method to print corresponding elements to stdout
+ */
+
+public class PrintArray {
+	public static void main(String[] args) {
+		
+		System.out.println("\nGenerics Learning Program");
+	}
+}
